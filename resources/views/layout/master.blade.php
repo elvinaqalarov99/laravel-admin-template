443f2ae221354f1c77b4ae41dc6a1eda6739ba77
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-16 15:49:12 +0400

    styles deleted

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -9,55 +9,17 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css" rel="stylesheet">
     <!-- Styles -->
-
-    {{-- fontawesome --}}
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.6/assets/owl.carousel.min.css" />
-    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-    <script src="https://cdn.boomcdn.com/libs/owl-carousel/2.3.4/owl.carousel.min.js"></script>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <!-- Flags Css-->
     <link href="{{ asset('assets/css/flags.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-    <div class="parent">
-        @include('layout/header')
-        @yield('content')
-    </div>
-
+    @include('layout/header')
+    @yield('content')
+    @include('layout/footer')
     <script src="{{ mix('js/app.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/owl.carousel.min.js"></script>
-
-    <script>
-        // $('.owl-carousel').owlCarousel({
-        //     loop: true,
-        //     margin: 10,
-        //     nav: false,
-        //     navText: [
-        //         "<i class='fa fa-caret-left'></i>",
-        //         "<i class='fa fa-caret-right'></i>"
-        //     ],
-        //     autoplay: true,
-        //     autoplayHoverPause: true,
-        //     responsive: {
-        //         0: {
-        //             items: 1
-        //         },
-        //         600: {
-        //             items: 3
-        //         },
-        //         1000: {
-        //             items: 3
-        //         }
-        //     }
-        // })
-    </script>
 </body>
 
 </html>
